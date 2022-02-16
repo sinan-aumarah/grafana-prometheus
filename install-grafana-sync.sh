4410cf7c4223e911f3c90fabc0ae91376ee173fb
Sinan Aumarah <saumarah@atlassian.com>
2022-02-17 10:53:06 +1100

    added script to sync dashboards

--- install-grafana-sync.sh
+++ install-grafana-sync.sh
@@ -3,8 +3,3 @@
 ## Reference https://github.com/mpostument/grafana-sync#push-dashboards
 wget https://github.com/mpostument/grafana-sync/releases/download/1.4.5/grafana-sync_1.4.5_Linux_x86_64.tar.gz
 tar -xvf grafana-sync_1.4.5_Linux_x86_64.tar.gz
-
-git clone git@github.com:atl-mk/Jira-DC-Grafana-Dashabords.git dashboards
-
-## make sure to export grafana API key (admin) as GRAFANA_API_KEY
-grafana-sync push-dashboards --apikey=$GRAFANA_API_KEY --directory="dashboards" --url http://127.0.0.1:3000
---
+++ sync-dashboards.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## USAGE: nohup ./sync-dashboards.sh > sync.log 2>&1 &
+
+SYNC_INTERVAL_IN_SECONDS=15
+
+## export env variable for the actual git repository
+git clone $DASHBOARDS_GIT_REPO dashboards
+
+function sync_dashboards {
+  while true
+  do
+    git -C ./dashboards pull
+    ## make sure to export grafana API key (admin) as GRAFANA_API_KEY
+    ./grafana-sync push-dashboards --apikey=$GRAFANA_API_KEY --directory="dashboards" --url http://127.0.0.1:3000
+
+    sleep $SYNC_INTERVAL_IN_SECONDS
+  done
+}
+
+sync_dashboards
+
