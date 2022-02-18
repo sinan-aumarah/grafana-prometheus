7cace0111e184641c5e82628cc0f6e7ed073b8be
Sinan Aumarah <saumarah@atlassian.com>
2022-02-18 15:42:37 +1100

    made grafana-sync script extract and set API key as well

--- install-grafana-sync.sh
+++ install-grafana-sync.sh
@@ -8,3 +8,10 @@ tar -xvf $GRAFANA_SYNC_ARTIFACT
 sudo cp grafana-sync /usr/local/bin
 
 rm -rf $GRAFANA_SYNC_ARTIFACT
+
+EXTRACTED_API_KEY=$(curl 'http://localhost:3000/api/auth/keys' -XPOST -uadmin:admin -H 'Content-Type: application/json' -d '{"role":"Admin","name":"my-auto-key"}' | \
+ python3 -c "import sys, json; print(json.load(sys.stdin)['key'])")
+
+echo "Extracted API KEY $EXTRACTED_API_KEY"
+
+export GRAFANA_API_KEY="$EXTRACTED_API_KEY"
