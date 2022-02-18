5b392add1d5b856de97f4f075a00dad5f0c677e5
Sinan Aumarah <saumarah@atlassian.com>
2022-02-18 13:53:40 +1100

    added grafana sync to user path

--- install-grafana-sync.sh
+++ install-grafana-sync.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+GRAFANA_SYNC_ARTIFACT=grafana-sync_1.4.5_Linux_x86_64.tar.gz
 ## Reference https://github.com/mpostument/grafana-sync#push-dashboards
-wget https://github.com/mpostument/grafana-sync/releases/download/1.4.5/grafana-sync_1.4.5_Linux_x86_64.tar.gz
-tar -xvf grafana-sync_1.4.5_Linux_x86_64.tar.gz
+wget https://github.com/mpostument/grafana-sync/releases/download/1.4.5/$GRAFANA_SYNC_ARTIFACT
+tar -xvf $GRAFANA_SYNC_ARTIFACT
+
+sudo cp grafana-sync /usr/local/bin
+
+rm -rf $GRAFANA_SYNC_ARTIFACT
