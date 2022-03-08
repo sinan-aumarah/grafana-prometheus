909a051a6e596fe84c541804cf75422b436b78c5
Sinan Aumarah <saumarah@atlassian.com>
2022-03-08 22:20:04 +1100

    made clone checkout target branch

--- sync-from-remote.sh
+++ sync-from-remote.sh
@@ -2,16 +2,17 @@
 
 ## USAGE: nohup ./sync-from-remote.sh > sync.log 2>&1 &
 TARGET_BRANCH="rsync"
+SYNC_DIR="dashboards"
 SYNC_INTERVAL_IN_SECONDS=30
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ## export env variable for the actual git repository
-git clone $DASHBOARDS_GIT_REPO dashboards
+git clone -b $TARGET_BRANCH $DASHBOARDS_GIT_REPO $SYNC_DIR
 
 function sync_dashboards {
   while true
   do
-    git -C ./dashboards pull origin $TARGET_BRANCH
+    git -C ./$SYNC_DIR pull origin $TARGET_BRANCH
     ## make sure to export grafana API key (admin) as GRAFANA_API_KEY
     "$SCRIPT_DIR"/grafana-sync push-dashboards --apikey=$GRAFANA_API_KEY --directory="dashboards" --url http://127.0.0.1:3000
 
