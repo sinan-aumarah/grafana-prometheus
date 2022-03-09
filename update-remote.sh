18d47f21f402ac0163976324f3c32639be530315
Sinan Aumarah <saumarah@atlassian.com>
2022-03-09 22:40:29 +1100

    removed usage of grafana-sync since it's relying on a buggy grafana SDK

--- update-remote.sh
+++ update-remote.sh
@@ -1,25 +1,56 @@
 #!/bin/sh
 
-## Run in crontab to run every midnight
-## 00 00 * * * path/to/update-remote.sh
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-SYNC_DIR="$SCRIPT_DIR/sync_directory"
-TARGET_BRANCH="auto-sync"
+
+API_KEY=$GRAFANA_API_KEY
+TARGET_DASHBOARDS_FOLDER="$SCRIPT_DIR/dashboards"
+TARGET_GIT_BRANCH="auto-sync"
+GRAFANA_URL="http://127.0.0.1:3000"
 
 current_timestamp() {
   date +"at %H:%M:%S on %d/%m/%Y"
 }
 
-## clone project if not cloned already
-if [ ! -d $SYNC_DIR ]
-then
-  git clone -b $TARGET_BRANCH $DASHBOARDS_GIT_REPO $SYNC_DIR
-fi
+export_dashboards() {
+  NUMBER_OF_GRAFANA_DASHBOARDS=0
+  for dash in $(curl -sSL -k -H "Authorization: Bearer ${API_KEY}" "${GRAFANA_URL}/api/search?query=&" | jq -r '.[] | select(.type == "dash-db") | .uid'); do
+    curl -sSL -k -H "Authorization: Bearer ${API_KEY}" "${GRAFANA_URL}/api/dashboards/uid/$dash" | jq -r . >${TARGET_DASHBOARDS_FOLDER}/${dash}.json
 
-"$SCRIPT_DIR"/grafana-sync pull-dashboards --apikey=$GRAFANA_API_KEY --directory="$SYNC_DIR" --url http://127.0.0.1:3000
+    # rename the exported dashboard to use slug rather than dashboard id
+    slug=$(cat ${TARGET_DASHBOARDS_FOLDER}/${dash}.json | jq -r '.meta.slug')
+    mv ${TARGET_DASHBOARDS_FOLDER}/${dash}.json "${TARGET_DASHBOARDS_FOLDER}/${slug}.json"
 
-cd $SYNC_DIR || exit
-git add .
-git commit -am "auto-sync $(current_timestamp)"
-git push origin $TARGET_BRANCH
+    # if we want to save the dashboards with their corresponding folder name
+    #folder=$(cat ${TARGET_DASHBOARDS_FOLDER}/${dash}.json | jq -r '.meta.folderTitle')
+    #mkdir -p "${TARGET_DASHBOARDS_FOLDER}/${folder}"
+    #mv ${TARGET_DASHBOARDS_FOLDER}/${dash}.json "${TARGET_DASHBOARDS_FOLDER}/${folder}/${slug}.json"
+    NUMBER_OF_GRAFANA_DASHBOARDS=$[$NUMBER_OF_GRAFANA_DASHBOARDS + 1]
+  done
+
+  echo "Exporting done. Saved $NUMBER_OF_GRAFANA_DASHBOARDS dashboards"
+}
+
+clone_remote_repo_if_not_present() {
+  ## clone project if not cloned already
+  if [ ! -d "$TARGET_DASHBOARDS_FOLDER" ]
+  then
+    git clone -b "$TARGET_GIT_BRANCH" "$DASHBOARDS_GIT_REPO" "$TARGET_DASHBOARDS_FOLDER"
+    git remote -v
+  fi
+}
+
+push_to_remote() {
+  cd "$TARGET_DASHBOARDS_FOLDER" || exit
+  git add .
+  git commit -am "auto-sync $(current_timestamp)"
+  git push origin $TARGET_GIT_BRANCH
+}
+
+
+#### START OF SCRIPT EXECUTION #####
+
+clone_remote_repo_if_not_present
+
+export_dashboards
+
+push_to_remote
