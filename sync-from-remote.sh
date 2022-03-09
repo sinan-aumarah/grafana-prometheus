3643920a96156c19447f2338c30da377266b04ea
Sinan Aumarah <saumarah@atlassian.com>
2022-03-09 23:20:42 +1100

    changed sync to use REST api as the grafana SDK is very flaky

--- sync-from-remote.sh
+++ sync-from-remote.sh
@@ -1,24 +1,55 @@
 #!/bin/bash
 
-## USAGE: nohup ./sync-from-remote.sh > sync.log 2>&1 &
-TARGET_BRANCH="auto-sync"
-SYNC_DIR="dashboards"
-SYNC_INTERVAL_IN_SECONDS=30
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-## export env variable for the actual git repository
+## USAGE: nohup ./sync-from-remote.sh > sync.log 2>&1 &
+TARGET_BRANCH="auto-sync"
+SYNC_DIR="$SCRIPT_DIR/remote-sync-dashboards"
+SYNC_INTERVAL_IN_SECONDS=30
+TARGET_HOST="127.0.0.1:3000"
+
 git clone -b $TARGET_BRANCH $DASHBOARDS_GIT_REPO $SYNC_DIR
 
-function sync_dashboards {
-  while true
-  do
-    git -C ./$SYNC_DIR pull
-    ## make sure to export grafana API key (admin) as GRAFANA_API_KEY
-    "$SCRIPT_DIR"/grafana-sync push-dashboards --apikey=$GRAFANA_API_KEY --directory="dashboards" --url http://127.0.0.1:3000
-
-    sleep $SYNC_INTERVAL_IN_SECONDS
+import_dashboards() {
+  
+  if [ -d "$SYNC_DIR" ]; then
+      DASH_LIST=$(find "$SYNC_DIR" -mindepth 1 -name \*.json)
+      if [ -z "$DASH_LIST" ]; then
+          echo "Directory $SYNC_DIR contains no JSON files! make sure you cloned a branch with some dashboards"
+          exit 1
+      fi
+  fi
+  
+  NUMBER_OF_SUCCESSFULL_IMPORTS=0
+  NUMBER_OF_FAILURES=0
+  COUNTER=0
+  
+  for DASH_FILE in $DASH_LIST; do
+      COUNTER=$((COUNTER + 1))
+      #echo "Importing $COUNTER/$FILESTOTAL: $DASH_FILE..."
+      RESULT=$(cat "$DASH_FILE" | jq '. * {overwrite: true, dashboard: {id: null}}' | curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $GRAFANA_API_KEY" "http://$TARGET_HOST/api/dashboards/db" -d @-)
+      if [[ "$RESULT" == *"success"* ]]; then
+          # echo "$RESULT"
+          NUMBER_OF_SUCCESSFULL_IMPORTS=$((NUMBER_OF_SUCCESSFULL_IMPORTS + 1))
+      else
+          echo "$RESULT"
+          NUMBER_OF_FAILURES=$((NUMBER_OF_FAILURES + 1))
+      fi
   done
+  
+  echo "Import complete. $NUMBER_OF_SUCCESSFULL_IMPORTS dashboards were successfully imported. $NUMBER_OF_FAILURES dashboard imports failed";
 }
 
-sync_dashboards
+
+#### START OF SCRIPT EXECUTION #####
+
+while true
+do
+  git -C $SYNC_DIR pull
+
+  import_dashboards
+
+  sleep $SYNC_INTERVAL_IN_SECONDS
+done
+
 
