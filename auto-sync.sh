1f22ead8cbbb7afaa2d58537868009dbbeb3fe8d
Sinan Aumarah <saumarah@atlassian.com>
2022-03-11 08:28:26 +1100

    changed syncing to every hour

--- auto-sync.sh
+++ auto-sync.sh
@@ -2,14 +2,14 @@
 
 ## USAGE: nohup ./auto-sync.sh > sync.log 2>&1 &
 
-SYNC_INTERVAL_IN_SECONDS=60
+SYNC_INTERVAL_IN_SECONDS=3600 # every 1hr
 
 while true
 do
 
   ./update-remote.sh
 
-  ./sync-from-remote.sh
+  #./sync-from-remote.sh
 
   sleep $SYNC_INTERVAL_IN_SECONDS
 done
